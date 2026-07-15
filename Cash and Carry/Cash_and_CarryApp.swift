cbf95a0aee6bfa499a0fc855956195f8d81a16d2
edilayehu <edilayehu@eaglelionsystems.com>
2026-07-15 09:35:21 +0300

    Initial Commit

---
+++ Cash and Carry/Cash_and_CarryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cash_and_CarryApp.swift
+//  Cash and Carry
+//
+//  Created by Edil on 15/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct Cash_and_CarryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
